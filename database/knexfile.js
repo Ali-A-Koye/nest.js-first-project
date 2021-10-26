5b9d2374e0871860ae3dc3f0772a5a35f9672d69
Brusk Hamarash <broosk.edogawa@gmail.com>
2021-10-27 00:35:14 +0300

    Refactors knexfile to use ESM import statement. DRYs ups

--- database/knexfile.js
+++ database/knexfile.js
@@ -1,40 +1,37 @@
-const path = require('path');
-require('dotenv').config({ path: path.join(__dirname, '../', '.env') });
+import path from 'path';
+import dotenv from 'dotenv';
+
+dotenv.config({ path: path.join(__dirname, '../', '.env') });
 
 const folders = {
-	seeds: {
-		directory: path.join(__dirname, './seeds/'),
-	},
-	migrations: {
-		tableName: 'nest-demo-migrations',
-		directory: path.join(__dirname, './migrations/'),
-	},
+  seeds: {
+    directory: path.join(__dirname, './seeds/'),
+  },
+  migrations: {
+    tableName: 'nest-demo-migrations',
+    directory: path.join(__dirname, './migrations/'),
+  },
 };
 
-module.exports = {
-	development: {
-		client: 'mysql',
-		connection: {
-			host: process.env.HOST,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_NAME,
-			debug: false,
-		},
-		port: 80,
-		...folders,
-	},
-	production: {
-		client: 'mysql',
-		connection: {
-			host: process.env.HOST,
-			user: process.env.DB_USER,
-			password: process.env.DB_PASSWORD,
-			database: process.env.DB_NAME,
-			debug: false,
-		},
-		port: 80,
-		...folders,
-	},
+const connection = {
+  host: process.env.DB__HOST,
+  user: process.env.DB__USER,
+  password: process.env.DB__PASSWORD,
+  database: process.env.DB__NAME,
+  debug: false,
+}
 
+module.exports = {
+  development: {
+    client: 'mysql',
+    connection,
+    port: 80,
+    ...folders,
+  },
+  production: {
+    client: 'mysql',
+    connection,
+    port: 80,
+    ...folders,
+  },
 };
