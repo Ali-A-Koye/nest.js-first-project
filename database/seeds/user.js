0fd218fb273743897aefd7626252cf48b6e8b1e6
Brusk Hamarash <broosk.edogawa@gmail.com>
2021-10-27 01:05:21 +0300

    Adds a default salt to the user.js seed

--- database/seeds/user.js
+++ database/seeds/user.js
@@ -1,13 +1,18 @@
-exports.seed = (knex) => knex('user').del()
-	.then(() => knex('user').insert([
-		{
-			id: 1,
-			name: 'First User',
-			username:"first@user",
-			password:"---",
-			active:1,
-			deleted:0,
-			created_at:knex.fn.now(),
-			created_by:0
-		},
-	]));
+exports.seed = (knex) =>
+  knex('user')
+    .del()
+    .then(() =>
+      knex('user').insert([
+        {
+          id: 1,
+          name: 'First User',
+          username: 'first@user',
+          password: '---',
+          salt: '1234',
+          active: 1,
+          deleted: 0,
+          created_at: knex.fn.now(),
+          created_by: 0,
+        },
+      ]),
+    );
