39f304966588ad90946dfb2f6c012c3dfe090376
Brusk Hamarash <broosk.edogawa@gmail.com>
2021-10-27 01:10:58 +0300

    Linting

--- src/auth/dto/auth.dto.ts
+++ src/auth/dto/auth.dto.ts
@@ -1,28 +1,22 @@
-import {
-    MinLength,
-   
-    IsString,
-   
-  } from 'class-validator';
-  
-  export class LoginDTO {
-    @IsString()
-    username: string;
-  
-    @MinLength(6)
-    @IsString()
-    password: string;
-  }
-  
-  export class SignupDTO {
-    @IsString()
-    name: string;
-  
-    @IsString()
-    username: string;
-  
-    @MinLength(6)
-    @IsString()
-    password: string;
-  }
-  
+import { MinLength, IsString } from 'class-validator';
+
+export class LoginDTO {
+  @IsString()
+  username: string;
+
+  @MinLength(6)
+  @IsString()
+  password: string;
+}
+
+export class SignupDTO {
+  @IsString()
+  name: string;
+
+  @IsString()
+  username: string;
+
+  @MinLength(6)
+  @IsString()
+  password: string;
+}
