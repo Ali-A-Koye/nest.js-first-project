734f75d31db8500a27d9ba0b52945e58836894a7
Brusk Hamarash <broosk.edogawa@gmail.com>
2021-10-27 01:00:03 +0300

    Improves JwtMiddleware

--- src/middlewares/jwtVerify.middlewares.ts
+++ src/middlewares/jwtVerify.middlewares.ts
@@ -13,17 +13,17 @@ import * as jwt from 'jsonwebtoken';
 @Injectable()
 export class JwtVerifyMiddleware implements NestMiddleware {
   constructor(private readonly userService: UserService) {}
-  async use(req: Request, res: Response, next: NextFunction) {
+  async use(req: any, res: Response, next: NextFunction) {
+
     try {
       const { authorization } = req.headers;
-
       if (!authorization)
-        next(new UnauthorizedException('Invalid Token Provided!'));
+        return next(new UnauthorizedException('Invalid Token Provided!')); // It's necessary to return here. Otherwise, code excution will continue and will throw an error.
+
       const parts = authorization.split(' ');
       if (parts[1]) {
         try {
           const decoded = jwt.verify(parts[1], process.env.JWT_SECRET);
-          // @ts-ignore
           req.user = decoded;
           next();
         } catch (err) {
@@ -35,7 +35,7 @@ export class JwtVerifyMiddleware implements NestMiddleware {
         next(new UnauthorizedException('invalid token provided!'));
       }
     } catch (err) {
-      next(new InternalServerErrorException());
+      next(err); // Just throw the exception that you instantiated above instead of Internal Server Error.
     }
   }
 }
